86b1cb8e100abbf94bae63955818dce66502e7c2
leoraggy <leoragual35@gmail.com>
2025-10-28 17:04:29 -0700

    Implemented getLongestWord with arraylist

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 public class PracticeTest {
@@ -19,7 +21,28 @@ public class PracticeTest {
     }
     
 
-    // TODO: Make tests for each problem you solve
+     /**
+     *  Find the longest word that starts with a specific character letter in.
+     * 
+     * @param myList a non-empty, non-null list of numbers
+     * @param target
+     * @return the difference between the largest and smallest number
+     */
+      public static String getLongestWord(ArrayList<String> myList, char target){
+        String currentLongest = "";
+        if(myList.get(0).charAt(0) == target){
+            currentLongest = myList.get(0);
+        }
+        
+        for(int i = 1; i < myList.size(); i++){
+            if(myList.get(i).length() > currentLongest.length() && myList.get(i).charAt(0) == target){
+                currentLongest = myList.get(i);
+            }
+        }
+
+        return currentLongest;
+
+    }
     
 }
 
