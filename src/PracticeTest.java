27876b0739c4256d1195da552cf010e7c83ea232
ryanrmills <ryanroymills@gmail.com>
2025-10-31 13:20:42 -0700

    I actually forgot to keep committing

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 public class PracticeTest {
@@ -20,7 +24,34 @@ public class PracticeTest {
     
 
     // TODO: Make tests for each problem you solve
-    
+    @Test
+    void testLongestWordSpecific(){
+        List<String> stringList = List.of("char", "collab", "charizard");
+        String actual = Practice.longestWordSpecificLetter(stringList, 'c');
+
+        assertEquals("charizard", actual);
+    }
+
+    @Test
+    void testWordsLongerThanMN(){
+        Set<String> stringSet = Set.of("public", "a", "the", "demonstrate", "collaborate");
+        int actual = Practice.wordsLongerThanMN(stringSet, 6, 7);
+        assertEquals(4, actual);
+    }
+
+    @Test
+    void testDiffEvenOdd(){
+        Map<String, Integer> stringMap = Map.of("even", 4, "odd", 5);
+        int actual = Practice.diffEvenOdd(stringMap);
+        assertEquals(1, actual);
+    }
+
+    @Test
+    void testSecondLargest(){
+        Map<Integer, Integer> intMap = Map.of(3, 4, 2, 5);
+        int actual = Practice.secondLargest(intMap);
+        assertEquals(2, actual);
+    }
 }
 
 
